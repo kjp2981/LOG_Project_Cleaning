7e87122fd3a88b9f7800eaad3e6e7eb8b2d79e0b
DESKTOP-OHD6DA9\USER <seoyun0700@daum.net>
2021-10-28 19:46:12 +0900

    사운드 조절 재커밋

--- Assets/2_Script/Manager/SoundManager.cs
+++ Assets/2_Script/Manager/SoundManager.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     public AudioSource musicSource;
+    public AudioSource soundSource;
 
     public void SetMusicVolume(float volume)
     {
         musicSource.volume = volume;
+    }
+    public void SetSoundVolume(float volume)
+    {
+        soundSource.volume = volume;
     }
 }
