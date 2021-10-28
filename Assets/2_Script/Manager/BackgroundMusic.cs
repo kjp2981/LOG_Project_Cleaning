f0ea709b2723cd563a25e9903178cd7bcff21cff
kjp29816265@gmail.com <kjp29816265@gmail.com>
2021-10-28 18:36:24 +0900

    오디오 작업 끝

--- Assets/2_Script/Manager/BackgroundMusic.cs
+++ Assets/2_Script/Manager/BackgroundMusic.cs
@@ -2,24 +2,20 @@
 
 public class BackgroundMusic : MonoBehaviour
 {
-    private AudioSource audioSource = null;
+    public AudioSource backAudioSource = null;
 
     [SerializeField]
     private AudioClip[] audioClips = null;
 
     private void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+        AudioChange();
     }
 
     public void AudioChange()
     {
-        if (GameManager.Instance.index == 1)
-            audioSource.clip = audioClips[0];
-        else if (GameManager.Instance.index == 2)
-            audioSource.clip = audioClips[1];
-        else
-            audioSource.clip = audioClips[2];
-        audioSource.Play();
+        backAudioSource.clip = audioClips[GameManager.Instance.index - 1];
+        backAudioSource.Stop();
+        backAudioSource.PlayOneShot(backAudioSource.clip);
     }
 }
